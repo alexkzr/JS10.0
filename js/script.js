c89e96283e311b87b9d040ecb95b63af15e2bc57
Aleksander Kozyrev <askozyrev@corp.oakmgmt.ru>
2019-10-18 16:49:33 +0300

    lesson 2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-let money = 5000; 
+let money = 5000;
 
-let income = true;
+let income = 'freelance';
 
-let addExpenses = 10000;
+let addExpenses = 'Интернет, Транспорт, Электричество, Квартплата';
 
-let deposit = 1; 
+let deposit = true;
 
-const mission = 'to serve and to prosper';
+const mission = 500000;
 
 const period = 2;
 
+let budgetDay = 20000;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(income.length);
+console.log('Период ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей.');
+console.log(addExpenses.toLowerCase());
+
 alert(addExpenses);
 console.log(addExpenses);
+console.log(addExpenses.split(','));
+console.log(budgetDay / 30);
+console.log(budgetDay % 30);
