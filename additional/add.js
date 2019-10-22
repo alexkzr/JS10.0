d8e8e6a0602f267606d19e158c621c62b711ff9f
Alex Kozyrev <alexkozyrev82@gmail.com>
2019-10-22 20:00:11 +0300

    lesson 4 additional finished

--- additional/add.js
+++ additional/add.js
@@ -1,6 +1,9 @@
 let excerpt = function (param) {
-  if (typeof param !== 'string') {
-    alert('Вы ввели не текст');
+  console.log('param: ', param);
+  console.log('param: ', param.length);
+
+  if (typeof param !== 'string' || param.length < 30) {
+    alert('Введите текст не менее 30 символов');
   } else {
     let result = '';
     result += param.substr(0, 29).trim();
@@ -8,5 +11,5 @@ let excerpt = function (param) {
     return document.write(result);
   }
 };
-let argument = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+let argument = 'Lorem dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 excerpt(argument);
