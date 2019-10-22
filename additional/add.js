bb9532e651301a36cbb7222cb30b2a702e296c2f
Alex Kozyrev <alexkozyrev82@gmail.com>
2019-10-22 20:16:33 +0300

    lesson 4 сделал правки куратора

--- additional/add.js
+++ additional/add.js
@@ -2,14 +2,17 @@ let excerpt = function (param) {
   console.log('param: ', param);
   console.log('param: ', param.length);
 
-  if (typeof param !== 'string' || param.length < 30) {
+  let result = '';
+  if (typeof param !== 'string') {
     alert('Введите текст не менее 30 символов');
-  } else {
-    let result = '';
+  } else if (param.length > 30 && typeof param === 'string'){
     result += param.substr(0, 29).trim();
     result += '...';
     return document.write(result);
+  } else if (param.length <= 30 && typeof param === 'string') {
+    result = param;
+    return document.write(result);
   }
 };
-let argument = 'Lorem dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+let argument = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 excerpt(argument);
