bf00f48a3aa5cc5212395a5411876f7dbcf9e40b
Alex Kozyrev <alexkozyrev82@gmail.com>
2019-10-25 19:54:32 +0300

    cleaned up

--- bonus1/script3.js
+++ bonus1/script3.js
@@ -6,7 +6,9 @@ let sum = 0;
 let runCode = function () {
   single = prompt('Введите цифру', 8);
   if (single == null) {
-    sum += +single;
+    // if (+single > 0) {
+    //   sum += +single;
+    // }
     console.log('Сумма чисел массива: : ', sum);
     return sum;
   } else if (isNaN(single) || single === '' && single != null) {
