13417f606852a1c9545d7dea1d83b05e88c92821
Alex Kozyrev <alexkozyrev82@gmail.com>
2019-11-09 21:28:44 +0300

    finished required lesson12

---
+++ js/script.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+
+  //Timer
+  function countTimer(deadline) {
+    let timerHours = document.querySelector('#timer-hours'),
+      timerMinutes = document.querySelector('#timer-minutes'),
+      timerSeconds = document.querySelector('#timer-seconds');
+
+    function getTimeRemaining() {
+      let dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor(timeRemaining / 60 / 60);
+
+      return { timeRemaining, hours, minutes, seconds };
+    }
+    function updateClock() {
+
+      let timer = getTimeRemaining();
+      timerHours.textContent = timer.hours;
+      timerMinutes.textContent = timer.minutes;
+      timerSeconds.textContent = timer.seconds;
+      if (timer.timeRemaining > 0) {
+        setInterval(updateClock, 1000);
+      } else if (timer.timeRemaining < 0) {
+        timerHours.textContent = 0;
+        timerMinutes.textContent = 0;
+        timerSeconds.textContent = 0;
+      }
+    }
+    updateClock();
+  }
+  countTimer('09 november 2019');
+});
