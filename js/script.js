27d109a660f9a854442806a8b0f43333e77836c6
Alex Kozyrev <alexkozyrev82@gmail.com>
2019-11-13 21:35:56 +0300

    finished lesson13

--- js/script.js
+++ js/script.js
@@ -33,53 +33,101 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     updateClock();
   }
-  countTimer(plusDay());
-});
 
-const plusDay = function () {
-  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Octover', 'November', 'December'];
-  let date = new Date();
-  let day = date.getDate() + 1;
-  let year = date.getFullYear();
-  let month = date.getMonth();
-  let monthName = months[month];
-  let hours = date.getHours();
-  let hoursTotal = (hours + 24) / 24;
-  let hoursRounded = hoursTotal.toFixed(2);
-  let moreDays = day * hoursRounded;
-  let twentyFour = new Date(new Date().getTime() + 60 * 60 * 24 * 1000);;
-  return twentyFour;
-};
-//End of Timer
-
-//Menu 
-const toggleMenu = () => {
-  const btnMenu = document.querySelector('.menu'),
-    menu = document.querySelector('menu'),
-    closeBtn = document.querySelector('.close-btn'),
-    menuItem = document.querySelectorAll('ul > li');
-  const menuAction = function () {
-    menu.classList.toggle('active-menu');
+  const plusDay = function () {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Octover', 'November', 'December'];
+    let date = new Date();
+    let day = date.getDate() + 1;
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let monthName = months[month];
+    let hours = date.getHours();
+    let hoursTotal = (hours + 24) / 24;
+    let hoursRounded = hoursTotal.toFixed(2);
+    let moreDays = day * hoursRounded;
+    let twentyFour = new Date(new Date().getTime() + 60 * 60 * 24 * 1000);;
+    return twentyFour;
   };
-  btnMenu.addEventListener('click', menuAction);
-  closeBtn.addEventListener('click', menuAction);
-  menuItem.forEach((elem) => elem.addEventListener('click', menuAction));
-};
-toggleMenu();
+  countTimer(plusDay());
 
-//popup
+  //End of Timer
 
-const togglePopup = function () {
+  //Menu 
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItem = document.querySelectorAll('ul > li');
+    const menuAction = function () {
+      menu.classList.toggle('active-menu');
+    };
+    btnMenu.addEventListener('click', menuAction);
+    closeBtn.addEventListener('click', menuAction);
+    menuItem.forEach((elem) => elem.addEventListener('click', menuAction));
+  };
+  toggleMenu();
+
+  //popup
   const popup = document.querySelector('.popup'),
     popupBtn = document.querySelectorAll('.popup-btn'),
     popupClose = document.querySelector('.popup-close');
-  popupBtn.forEach((elem) => {
-    elem.addEventListener('click', () => {
+  const checkScreen = function () {
+    if (window.screen.width < 768) {
+      popup.style.transition = 'all 0s ease 0s';
       popup.style.display = 'block';
+      popup.style.transform = 'translateY(0)';
+
+    } else {
+      popup.style.transition = 'all 0.5s';
+      if (!popup.style.transform || popup.style.transform === 'translateY(-100%)') {
+        popup.style.display = 'block';
+        popup.style.transform = 'translateY(0)';
+      } else {
+        popup.style.transform = 'translateY(-100%)';
+
+      }
+    }
+  };
+  const togglePopup = function () {
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        checkScreen();
+      });
+
     });
-  });
-  popupClose.addEventListener('click', () => {
-    popup.style.display = 'none';
-  });
-};
-togglePopup();
+    popupClose.addEventListener('click', () => {
+      // popup.style.display = 'none';
+      popup.style.transform = 'translateY(-100%)';
+
+    });
+  };
+  togglePopup();
+
+  //smooth scroll down
+
+  let linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
+    V = 1;  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+  for (let i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function (e) { //по клику на ссылку
+      e.preventDefault(); //отменяем стандартное поведение
+      let w = window.pageYOffset,  // производим прокрутка прокрутка
+        hash = this.href.replace(/[^#]*(.*)/, '$1');  // к id элемента, к которому нужно перейти
+      let t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+        start = null;
+      requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+      function step(time) {
+        if (start === null) start = time;
+        let progress = time - start,
+          r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+        window.scrollTo(0, r);
+        if (r != w + t) {
+          requestAnimationFrame(step)
+        } else {
+          location.hash = hash;  // URL с хэшем
+        }
+      }
+    }, false);
+  }
+
+
+});
