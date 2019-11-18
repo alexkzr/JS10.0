7284e1e301d93e4c066881e12abdce2f6d935e86
Alex Kozyrev <alexkozyrev82@gmail.com>
2019-11-18 21:08:56 +0300

    finished input validation

--- js/script.js
+++ js/script.js
@@ -321,10 +321,21 @@ window.addEventListener('DOMContentLoaded', function () {
    *  Input validation         *
   \*************************** */
 
+  let inputValue = 0;
   let calcBlock = document.querySelectorAll('.calc-block input');
   calcBlock.forEach((item) => {
-    item.setAttribute("pattern", "/d/i");
-  })
+    item.addEventListener('keydown', function (e) {
+      let key = e.keyCode ? e.keyCode : e.which;
+
+      if (!([8, 9, 13, 27, 46, 110, 190].indexOf(key) !== -1 ||
+        (key == 65 && (e.ctrlKey || e.metaKey)) ||
+        (key >= 35 && key <= 40) ||
+        (key >= 48 && key <= 57 && !(e.shiftKey || e.altKey)) ||
+        (key >= 96 && key <= 105)
+      )) e.preventDefault();
+    });
+  });
+
 
 
 });
