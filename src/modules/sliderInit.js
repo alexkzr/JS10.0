02d7a256edcf9f367894aa2f55a6b0bb18a585f6
Alex Kozyrev <alexkozyrev82@gmail.com>
2019-11-29 20:58:27 +0300

    fixed popup

---
+++ src/modules/sliderInit.js
@@ -0,0 +1,32 @@
+import SliderCarousel from "./modules/bigSlider";
+
+const sliderInit = () => {
+  const carousel = new SliderCarousel({
+    main: '.companies-wrapper',
+    wrap: '.companies-hor',
+    slidesToShow: 4,
+    infinity: true,
+
+    responsive: [
+      {
+        breakpoint: 1200,
+        slidesToShow: 4,
+      },
+      {
+        breakpoint: 1024,
+        slidesToShow: 3,
+
+      },
+      {
+        breakpoint: 768,
+        slidesToShow: 2,
+
+      },
+      {
+        breakpoint: 576,
+        slidesToShow: 1,
+
+      }]
+  });
+  carousel.init();
+}
